0de229fa74d563f5b5c0a400adbe776c8e2617bb
namanmanchanda09 <naman.mohan09@gmail.com>
2020-11-14 18:55:18 +0530

    image picker added

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -7,19 +7,34 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var nameText: UITextField!
     @IBOutlet var artistName: UITextField!
     @IBOutlet var yearName: UITextField!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hidekeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        view.addGestureRecognizer(imageTapRecognizer)
+    }
+    
+    @objc func selectImage(){
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func hidekeyboard(){
@@ -27,8 +42,12 @@ class DetailsVC: UIViewController {
     }
     
     @IBAction func saveButtonClicked(_ sender: UIButton) {
+        print("SAVED")
         
     }
     
 
 }
+
+
+
